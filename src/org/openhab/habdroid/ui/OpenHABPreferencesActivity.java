5264c2fc7ccae240ce5f5cae0e3a0e7b6d71bb54
belovictor <belovictor@gmail.com>
2013-03-13 02:19:38 +0400

    Current values of preferences are now shown in Settings menu of HABDroid

--- src/org/openhab/habdroid/ui/OpenHABPreferencesActivity.java
+++ src/org/openhab/habdroid/ui/OpenHABPreferencesActivity.java
@@ -51,40 +51,87 @@ import android.os.Bundle;
 
 public class OpenHABPreferencesActivity extends PreferenceActivity {
 	@SuppressWarnings("deprecation")
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    addPreferencesFromResource(R.xml.preferences);
 	    Preference urlPreference = getPreferenceScreen().findPreference("default_openhab_url");
 	    Preference altUrlPreference = getPreferenceScreen().findPreference("default_openhab_alturl");
+	    Preference usernamePreference = getPreferenceScreen().findPreference("default_openhab_username");
+	    Preference passwordPreference = getPreferenceScreen().findPreference("default_openhab_password");
 	    urlPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object newValue) {
 				Log.d("OpenHABPreferencesActivity", "Validating new url = " + (String)newValue);
 				String newUrl = (String)newValue;
-				if (newUrl.length() == 0)
-					return true;
-				if (urlIsValid(newUrl)) {
+				if (newUrl.length() == 0 || urlIsValid(newUrl)) {
+					updateTextPreferenceSummary(preference, (String)newValue);
 					return true;
 				}
 				showAlertDialog(getString(R.string.erorr_invalid_url));
 				return false;
 			}
 	    });
+	    updateTextPreferenceSummary(urlPreference, null);
 	    altUrlPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object newValue) {
 				String newUrl = (String)newValue;
-				if (newUrl.length() == 0)
-					return true;
-				if (urlIsValid(newUrl)) {
+				if (newUrl.length() == 0 || urlIsValid(newUrl)) {
+					updateTextPreferenceSummary(preference, (String)newValue);
 					return true;
 				}
 				showAlertDialog(getString(R.string.erorr_invalid_url));
 				return false;
 			}
 	    });
+	    updateTextPreferenceSummary(altUrlPreference, null);
+	    usernamePreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				updateTextPreferenceSummary(preference, (String)newValue);
+				return true;
+			}
+	    });
+	    updateTextPreferenceSummary(usernamePreference, null);
+	    passwordPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				updatePasswordPreferenceSummary(preference, (String)newValue);
+				return true;
+			}
+	    });
+	    updatePasswordPreferenceSummary(passwordPreference, null);
 	    setResult(RESULT_OK);
+	}
+	
+	private void updateTextPreferenceSummary(Preference textPreference, String newValue) {
+		if (newValue == null) {
+			if (textPreference.getSharedPreferences().getString(textPreference.getKey(), "").length() > 0)
+		    	textPreference.setSummary(textPreference.getSharedPreferences().getString(textPreference.getKey(), ""));
+		    else
+		    	textPreference.setSummary("Not set");
+		} else {
+			if (newValue.length() > 0)
+				textPreference.setSummary(newValue);
+			else
+				textPreference.setSummary("Not set");
+		}
+	}
+	
+	private void updatePasswordPreferenceSummary(Preference passwordPreference, String newValue) {
+		if (newValue == null) {
+			if (passwordPreference.getSharedPreferences().getString(passwordPreference.getKey(), "").length() > 0)
+				passwordPreference.setSummary("******");
+			else
+				passwordPreference.setSummary("Not set");
+		} else {
+			if (newValue.length() > 0)
+				passwordPreference.setSummary("******");
+			else
+				passwordPreference.setSummary("Not set");
+		}
 	}
 	
 	private boolean urlIsValid(String url) {
