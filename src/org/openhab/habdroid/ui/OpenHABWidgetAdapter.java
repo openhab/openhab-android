6099fcd01c03f399c25e568e5fee588dcacbdb5d
Victor Belov <belovictor@gmail.com>
2014-01-05 03:23:30 +0400

    Another fix for sitemap widget colors

--- src/org/openhab/habdroid/ui/OpenHABWidgetAdapter.java
+++ src/org/openhab/habdroid/ui/OpenHABWidgetAdapter.java
@@ -208,20 +208,22 @@ public class OpenHABWidgetAdapter extends ArrayAdapter<OpenHABWidget> {
                     widgetImage.clearColorFilter();
             }
         }
+        TextView defaultTextView = new TextView(widgetView.getContext());
         // Get TextView for widget label and set it's color
         labelTextView = (TextView)widgetView.findViewById(R.id.widgetlabel);
         if(labelColor != null && labelTextView != null) {
             Log.d(TAG, String.format("Setting label color to %d", labelColor));
             labelTextView.setTextColor(labelColor);
         } else if (labelTextView != null)
-            labelTextView.setTextColor(labelTextView.getTextColors().getDefaultColor());
+            labelTextView.setTextColor(defaultTextView.getTextColors().getDefaultColor());
         // Get TextView for widget value and set it's color
         valueTextView = (TextView)widgetView.findViewById(R.id.widgetvalue);
         if (valueColor != null && valueTextView != null) {
             Log.d(TAG, String.format("Setting value color to %d", valueColor));
             valueTextView.setTextColor(valueColor);
         } else if (valueTextView != null)
-            valueTextView.setTextColor(valueTextView.getTextColors().getDefaultColor());
+            valueTextView.setTextColor(defaultTextView.getTextColors().getDefaultColor());
+        defaultTextView = null;
     	switch (getItemViewType(position)) {
     	case TYPE_FRAME:
     		if (labelTextView != null)
