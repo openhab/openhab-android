1794b54454db55eeee457030af23c77f010ba37e
CristianSarac <sarac.cristian@gmail.com>
2016-03-31 03:31:10 +0200

    Added license header to all the files  Signed-off-by: Cristian Sarac <sarac.cristian@gmaill.com> (Github: CristianSarac)

--- mobile/src/main/java/org/openhab/habdroid/ui/widget/SegmentedControlButton.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/widget/SegmentedControlButton.java
@@ -1,13 +1,11 @@
 /**
- * Copyright (c) 2010-2014, openHAB.org and others.
+ * Copyright (c) 2010-2016, openHAB.org and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- *  @author Victor Belov
- *  @since 1.4.0
  *
  * This class is based on SegmentedControlButton by Benjamin Ferrari
  * http://bookworm.at
