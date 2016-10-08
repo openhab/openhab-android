f941ed74434d7e7cd778184bccfb585f7d89891f
Brenda Wallace <brenda@wallace.net.nz>
2016-10-08 19:50:11 +1300

    simplified some logic in overridePendingTransition()

--- mobile/src/main/java/org/openhab/habdroid/util/Util.java
+++ mobile/src/main/java/org/openhab/habdroid/util/Util.java
@@ -41,15 +41,16 @@ public class Util {
     private final static String TAG = Util.class.getSimpleName();
 
     public static void overridePendingTransition(Activity activity, boolean reverse) {
-        if (PreferenceManager.getDefaultSharedPreferences(activity).getString(Constants.PREFERENCE_ANIMATION, "android").equals("android")) {
-        } else if (PreferenceManager.getDefaultSharedPreferences(activity).getString(Constants.PREFERENCE_ANIMATION, "android").equals("ios")) {
-            if (reverse) {
-                activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        if (!PreferenceManager.getDefaultSharedPreferences(activity).getString(Constants.PREFERENCE_ANIMATION, "android").equals("android")) {
+            if (PreferenceManager.getDefaultSharedPreferences(activity).getString(Constants.PREFERENCE_ANIMATION, "android").equals("ios")) {
+                if (reverse) {
+                    activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                } else {
+                    activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
             } else {
-                activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                activity.overridePendingTransition(0, 0);
             }
-        } else {
-            activity.overridePendingTransition(0, 0);
         }
     }
 
