ab64c99df12b6117009eb66c815c96178058f8f8
Dan Cunningham <dan@digitaldan.com>
2017-04-08 17:21:39 -0700

    Revert "Update to latest API+tools+gradle+misc. (#268)" (#280)
    This reverts commit 7e405c5e20ae97fc924146038decea68a5b3c827.

--- mobile/src/main/java/org/openhab/habdroid/ui/OpenHABPreferencesActivity.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/OpenHABPreferencesActivity.java
@@ -22,13 +22,10 @@ import android.preference.PreferenceActivity;
 import android.security.KeyChain;
 import android.security.KeyChainAliasCallback;
 import android.security.KeyChainException;
-import android.security.keystore.KeyProperties;
 import android.util.Log;
-import java.text.DateFormat;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 
-import org.openhab.habdroid.BuildConfig;
 import org.openhab.habdroid.R;
 import org.openhab.habdroid.util.Constants;
 import org.openhab.habdroid.util.Util;
@@ -36,9 +33,6 @@ import org.openhab.habdroid.util.Util;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.cert.X509Certificate;
-import java.util.Locale;
-
-import static android.R.attr.format;
 
 /**
  * This is a class to provide preferences activity for application.
@@ -115,8 +109,6 @@ public class OpenHABPreferencesActivity extends PreferenceActivity {
         });
         updatePasswordPreferenceSummary(passwordPreference, null);
         updateTextPreferenceSummary(versionPreference, null);
-        versionPreference.setSummary(versionPreference.getSummary() + " - " +
-                DateFormat.getDateTimeInstance().format(BuildConfig.buildTime));
 
 
         updateSslCleintCertSumary(sslClientCert);
@@ -138,21 +130,12 @@ public class OpenHABPreferencesActivity extends PreferenceActivity {
 
                 sslClientCert.getSharedPreferences().edit().putString(sslClientCert.getKey(), null).apply();
 
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
-                    KeyChain.choosePrivateKeyAlias(OpenHABPreferencesActivity.this,
-                            keyChainAliasCallback,
-                            new String[]{"RSA", "DSA"},
-                            null,
-                            getPreferenceString(altUrlPreference, null),
-                            -1, null);
-                } else {
-                    KeyChain.choosePrivateKeyAlias(OpenHABPreferencesActivity.this,
-                            keyChainAliasCallback,
-                            new String[]{KeyProperties.KEY_ALGORITHM_RSA, KeyProperties.KEY_ALGORITHM_EC},
-                            null,
-                            Uri.parse(getPreferenceString(altUrlPreference, null)),
-                            null);
-                }
+                KeyChain.choosePrivateKeyAlias(OpenHABPreferencesActivity.this,
+                        keyChainAliasCallback,
+                        new String[]{"RSA", "DSA"},
+                        null,
+                        getPreferenceString(altUrlPreference, null),
+                        -1, null);
 
                 return true;
             }
