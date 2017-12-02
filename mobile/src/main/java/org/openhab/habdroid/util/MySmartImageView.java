d77f7db6e678a5c9a238cc2f10eb6a678e582e9b
lolodomo <lg.hc@free.fr>
2017-12-02 14:51:39 +0100

    Fixed image refresh to avoid triggering a new refresh while the previous is still in progress
    A minimum refresh rate is set to 100 ms.
    
    Fixes #420
    Fixes #459
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- mobile/src/main/java/org/openhab/habdroid/util/MySmartImageView.java
+++ mobile/src/main/java/org/openhab/habdroid/util/MySmartImageView.java
@@ -16,6 +16,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 
 import com.loopj.android.image.SmartImageView;
+import com.loopj.android.image.SmartImageTask;
+import com.loopj.android.image.SmartImage;
 
 import java.lang.ref.WeakReference;
 import java.util.Timer;
@@ -36,9 +38,25 @@ public class MySmartImageView extends SmartImageView {
 
         public void handleMessage(Message msg) {
             MySmartImageView imageView = viewWeakReference.get();
-            if (imageView != null) {
+            if (imageView != null && !imageView.refreshDisabled && imageView.myImageUrl != null) {
                 Log.i(TAG, "Refreshing image at " + imageView.myImageUrl);
-                imageView.setImage(new MyWebImage(imageView.myImageUrl, false, imageView.username, imageView.password));
+                imageView.refreshDisabled = true;
+                imageView.setImage(new MyWebImage(imageView.myImageUrl, false, imageView.username, imageView.password), imageView.imageCompletionListener);
+            }
+        }
+    }
+
+    private static class OnCompleteListener extends SmartImageTask.OnCompleteListener {
+        private final WeakReference<MySmartImageView> viewWeakReference;
+
+        OnCompleteListener(MySmartImageView smartImageView) {
+            viewWeakReference = new WeakReference<>(smartImageView);
+        }
+
+        public void onComplete(){
+            MySmartImageView imageView = viewWeakReference.get();
+            if (imageView != null) {
+                imageView.refreshDisabled = false;
             }
         }
     }
@@ -46,54 +64,70 @@ public class MySmartImageView extends SmartImageView {
     private String myImageUrl;
     private String username;
     private String password;
+    private boolean refreshDisabled;
 
     private Timer imageRefreshTimer;
+    private OnCompleteListener imageCompletionListener;
 
     public MySmartImageView(Context context) {
         super(context);
+        this.imageCompletionListener = new OnCompleteListener(this);
     }
 
     public MySmartImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.imageCompletionListener = new OnCompleteListener(this);
     }
 
     public MySmartImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.imageCompletionListener = new OnCompleteListener(this);
     }
 
     public void setImageUrl(String url, String username, String password) {
         this.myImageUrl = url;
         this.username = username;
         this.password = password;
-        setImage(new MyWebImage(url, username, password));
+        this.refreshDisabled = true;
+        setImage(new MyWebImage(url, username, password), imageCompletionListener);
     }
 
     public void setImageUrl(String url, final Integer fallbackResource, String username, String password) {
         this.myImageUrl = url;
         this.username = username;
         this.password = password;
-        setImage(new MyWebImage(url, username, password), fallbackResource);
+        this.refreshDisabled = true;
+        setImage(new MyWebImage(url, username, password), fallbackResource, imageCompletionListener);
     }
 
     public void setImageUrl(String url, final Integer fallbackResource, final Integer loadingResource, String username, String password) {
         this.myImageUrl = url;
         this.username = username;
         this.password = password;
-        setImage(new MyWebImage(url, username, password), fallbackResource, loadingResource);
+        this.refreshDisabled = true;
+        setImage(new MyWebImage(url, username, password), fallbackResource, loadingResource, imageCompletionListener);
     }
 
     public void setImageUrl(String url, boolean useImageCache, String username, String password) {
         this.myImageUrl = url;
         this.username = username;
         this.password = password;
-        setImage(new MyWebImage(url, useImageCache, username, password));
+        this.refreshDisabled = true;
+        setImage(new MyWebImage(url, useImageCache, username, password), imageCompletionListener);
+    }
+
+    public void setImageWithData(SmartImage image) {
+        this.myImageUrl = null;
+        this.username = null;
+        this.password = null;
+        this.refreshDisabled = true;
+        setImage(image, imageCompletionListener);
     }
 
     public void setRefreshRate(int msec) {
         Log.i(TAG, "Setting image refresh rate to " + msec + " msec for " + myImageUrl);
-        if (this.imageRefreshTimer != null) {
-            this.imageRefreshTimer.cancel();
-        }
+
+        cancelRefresh();
 
         this.imageRefreshTimer = new Timer();
         final Handler timerHandler = new RefreshHandler(this);
@@ -110,6 +144,7 @@ public class MySmartImageView extends SmartImageView {
         Log.i(TAG, "Cancel image Refresh for " + myImageUrl);
         if (this.imageRefreshTimer != null) {
             this.imageRefreshTimer.cancel();
+            this.refreshDisabled = false;
         }
     }
 }
