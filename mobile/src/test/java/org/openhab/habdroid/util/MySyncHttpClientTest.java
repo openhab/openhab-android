855e85c5e7f8fb68d91119405095d0394b815d71
Florian <florian.schmidt.welzow@t-online.de>
2017-12-29 15:44:41 +0100

    Fix MySyncHttpClientTest
    Signed-off-by: Florian <florian.schmidt.welzow@t-online.de>

--- mobile/src/test/java/org/openhab/habdroid/util/MySyncHttpClientTest.java
+++ mobile/src/test/java/org/openhab/habdroid/util/MySyncHttpClientTest.java
@@ -1,19 +1,10 @@
 package org.openhab.habdroid.util;
 
 
-import java.net.UnknownHostException;
-import java.util.HashMap;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import okhttp3.Call;
-import okhttp3.Headers;
-import okhttp3.Response;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +13,17 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.net.UnknownHostException;
+import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.Headers;
+import okhttp3.Response;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(PreferenceManager.class)
@@ -44,6 +46,7 @@ public class MySyncHttpClientTest {
     @Test
     public void testMethodErrorResponse() {
         MySyncHttpClient httpClient = new MySyncHttpClient(null,false, true);
+        httpClient.setBaseUrl("https://demo.test");
 
         String host = "just.a.local.url.local";
         Response resp = httpClient.method(
