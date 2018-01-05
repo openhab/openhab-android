c32c62ffa354f3e03aa987f1ba0be6e23f29fb66
mueller-ma <mueller-ma@users.noreply.github.com>
2018-01-05 13:18:34 +0100

    Use accent color in notifications
    Fixes #603
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/full/java/org/openhab/habdroid/core/GcmIntentService.java
+++ mobile/src/full/java/org/openhab/habdroid/core/GcmIntentService.java
@@ -19,6 +19,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -102,9 +103,10 @@ public class GcmIntentService extends IntentService {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_openhab_appicon_24dp)
-                        .setContentTitle("openHAB")
+                        .setContentTitle(getBaseContext().getString(R.string.app_name))
                         .setStyle(new NotificationCompat.BigTextStyle()
                                 .bigText(msg))
+                        .setColor(ContextCompat.getColor(this, R.color.openhab_orange))
                         .setAutoCancel(true)
                         .setSound(alarmSound)
                         .setContentText(msg)
