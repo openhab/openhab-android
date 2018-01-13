ae617f637fcb647873a9527a43771921ed89894d
mueller-ma <mueller-ma@users.noreply.github.com>
2018-01-13 18:37:17 +0100

    Update validate-play-store-description.sh

--- validate-play-store-description.sh
+++ validate-play-store-description.sh
@@ -3,32 +3,30 @@
 # Checks if play store descriptions does not exceed char limits
 # https://support.google.com/googleplay/android-developer/answer/113469?hl=en
 
-cd fastlane/metadata/android
-
 error=0
 
-for folder in *
+for folder in fastlane/metadata/android
 do
+    [ ! -d "$folder" ] && continue
     cd "$folder"
-    if [ -f "full_description.txt" ]
+    if [ -f "${folder}/full_description.txt" ]
     then
-        chars=$(wc -m full_description.txt | cut -d " " -f 1)
+        chars=$(wc -m "${folder}/full_description.txt" | cut -d " " -f 1)
         if [ "$chars" -gt 4000 ]
         then
             echo "Full descriptions in $folder exceeds 4000 char limit"
             let error++
         fi
     fi
-    if [ -f "short_description.txt" ]
+    if [ -f "${folder}/short_description.txt" ]
     then
-        chars=$(wc -m short_description.txt | cut -d " " -f 1)
+        chars=$(wc -m "${folder}/short_description.txt" | cut -d " " -f 1)
         if [ "$chars" -gt 80 ]
         then
             echo "Short descriptions in $folder exceeds 80 char limit"
             let error++
         fi
     fi
-    cd ..
 done
 
 if [ "$error" -ne 0 ]
