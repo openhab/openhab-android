35090986c3d172424435981295e4f49782a804e3
Florian <florian.schmidt.welzow@t-online.de>
2018-01-13 21:05:41 +0100

    Simplify layout and viewpager hierarchy
    Instead of implementing our own ViewPager to implement features we do not
    currently use (swiping pages) or where (imho) the benefit is at least
    questionable, use the default ViewPager delivered with the support
    library.
    
    Also: Do not maintain a main activity layout for API level 21, where the
    only difference to the one used for all other api levels is, that one
    property is added. This property can be added to the main layout file and
    will simply be ignored in api levels, which do not support it.
    
    Also: Remove the main activity twopane layout file, which is unused in the
    whole project.
    
    Signed-off-by: Florian <florian.schmidt.welzow@t-online.de>

--- mobile/src/main/java/org/openhab/habdroid/ui/OpenHABFragmentPagerAdapter.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/OpenHABFragmentPagerAdapter.java
@@ -17,7 +17,6 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 
 import org.openhab.habdroid.core.notifications.NotificationSettings;
-import org.openhab.habdroid.model.thing.ThingType;
 
 import java.util.ArrayList;
 import java.util.List;
