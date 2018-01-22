fdf34676ab983e64345ceb7ae6dbd487725bcc9a
mueller-ma <mueller-ma@users.noreply.github.com>
2018-01-22 18:18:00 +0100

    Use https for all links
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- assets/store_descriptions/en-US/strings.sh
+++ assets/store_descriptions/en-US/strings.sh
@@ -6,7 +6,7 @@ whatis='openHAB is a Java-based open-source home automation platform that integr
 rules='On top of the unified abstraction layer all connected devices are available for the overarching automation rule engines and different user interfaces.'
 
 supported='Supported Products'
-bindings='Over 200 specific add-ons provide support for brands, devices, technologies and communication protocols. Examples are Z-Wave, Philips Hue, Amazon Echo, Chromecast and Sonos. Discover all available add-ons and supported devices and/or functions at: http://docs.openhab.org/addons/bindings.html'
+bindings='Over 200 specific add-ons provide support for brands, devices, technologies and communication protocols. Examples are Z-Wave, Philips Hue, Amazon Echo, Chromecast and Sonos. Discover all available add-ons and supported devices and/or functions at: https://docs.openhab.org/addons/bindings.html'
 
 home_automation_solutions='Home Automation Solutions'
 lighting='Lighting'
@@ -23,7 +23,7 @@ report_issues='When you found an issue please report it in the forum or at https
 translation='If you want to translate the app into your language join us at https://crowdin.com/project/openhab-android'
 
 foundation='openHAB Foundation'
-about_foundation='The openHAB Foundation e.V. is a nonprofit organization with the mission to educate the public about the possibilities and benefits of free and open smart home solutions. Learn about the mission and the services of the foundation under http://www.openhabfoundation.org'
+about_foundation='The openHAB Foundation e.V. is a nonprofit organization with the mission to educate the public about the possibilities and benefits of free and open smart home solutions. Learn about the mission and the services of the foundation under https://www.openhabfoundation.org'
 
 important_note='Important note'
 oh_server='You need a openHAB server for this app'
