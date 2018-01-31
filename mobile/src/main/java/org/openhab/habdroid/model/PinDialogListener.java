db938838f69183b0b17a15f120a1560fa455dff1
Florian Schulte <mail@florian-schulte.net>
2018-01-31 21:08:42 +0100

    Revert "merged upstream"
    This reverts commit 7e2e87f17826cb6b60479db58caca823cf2c3865, reversing
    changes made to b95b962c1ded3f2680dbae46815954365697a0bf.

--- mobile/src/main/java/org/openhab/habdroid/model/PinDialogListener.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.habdroid.model;
-
-/**
- * Created by redeye on 13.08.16.
- */
-public interface PinDialogListener {
-    public void onPinEntered(String pin);
-    public void onPinAborted();
-}
