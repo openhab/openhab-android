d602b6f2d15aea720a29889f5e01f7dd7ff6b9c7
mueller-ma <mueller-ma@users.noreply.github.com>
2018-02-10 18:33:43 +0100

    Simplify intro code and change font (#700)
    * Use default font in intro slides
    
    Get rid of the stactraces, when the font is not found.
    
    Fixes #697
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Add StringRes annotation
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Simplify code
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Add JavaDoc
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/IntroActivity.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/IntroActivity.java
@@ -3,7 +3,9 @@ package org.openhab.habdroid.ui;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
@@ -11,54 +13,68 @@ import com.github.paolorotolo.appintro.AppIntroFragment;
 import org.openhab.habdroid.R;
 
 public class IntroActivity extends AppIntro {
+    int colorOpenHABOrange;
+    int colorOpenHABOrangeDark;
+    int colorGrey;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        colorOpenHABOrange = ContextCompat.getColor(this, R.color.openhab_orange);
+        colorOpenHABOrangeDark = ContextCompat.getColor(this, R.color.openhab_orange_dark);
+        colorGrey = ContextCompat.getColor(this, R.color.grey_300);
+
         // Add slides
         addOHSlide(R.string.intro_welcome,
-                R.string.intro_whatis, R.drawable
-                        .ic_openhab_appicon_340dp);
-        addOHSlide(R.string.intro_themes, R.string.intro_themes_description, R.drawable.themes);
-        addOHSlide(R.string.settings_openhab_demomode, R.string.info_demo_mode, R.drawable.demo_mode);
+                R.string.intro_whatis,
+                R.drawable.ic_openhab_appicon_340dp);
+        addOHSlide(R.string.intro_themes,
+                R.string.intro_themes_description,
+                R.drawable.themes);
+        addOHSlide(R.string.settings_openhab_demomode,
+                R.string.info_demo_mode,
+                R.drawable.demo_mode);
         // Change bar color
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            setBarColor(getColor(R.color.openhab_orange));
-            setSeparatorColor(getColor(R.color.openhab_orange_dark));
-        } else {
-            setBarColor(getResources().getColor(R.color.openhab_orange));
-            setSeparatorColor(getResources().getColor(R.color.openhab_orange_dark));
-        }
+        setBarColor(colorOpenHABOrange);
+        setSeparatorColor(colorOpenHABOrangeDark);
     }
 
+    /**
+     * Must be overridden to ensure that the intro will be closed when clicking on "SKIP"
+     * @param currentFragment
+     */
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
         finish();
     }
 
+    /**
+     * Must be overridden to ensure that the intro will be closed when clicking on "DONE"
+     * @param currentFragment
+     */
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
         finish();
     }
 
-    @Override
-    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
-        super.onSlideChanged(oldFragment, newFragment);
-        // Do something when the slide changes.
-    }
+    /**
+     * Add slide with fixed fonts and colors
+     * @param title
+     * @param description
+     * @param imageDrawable
+     */
+    private void addOHSlide(@StringRes int title, @StringRes int description,
+                            @DrawableRes int imageDrawable) {
+            addSlide(AppIntroFragment.newInstance(getString(title),
+                    null,       /* Title font: null => default */
+                    getString(description), /* Description */
+                    null,       /* Description font: null => default */
+                    imageDrawable,          /* Image */
+                    colorGrey,              /* Background color */
+                    colorOpenHABOrange,     /* Title color */
+                    colorOpenHABOrange));   /* Description color */
 
-    private void addOHSlide(int title, int description, @DrawableRes int imageDrawable) {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            addSlide(AppIntroFragment.newInstance(getString(title), "Helvetica",
-                    getString(description), "Helvetica", imageDrawable, getColor(R.color.grey_300),
-                    getColor(R.color.openhab_orange), getColor(R.color.openhab_orange)));
-        } else {
-            addSlide(AppIntroFragment.newInstance(getString(title), "Helvetica",
-                    getString(description), "Helvetica", imageDrawable,
-                    getResources().getColor(R.color.grey_300),
-                    getResources().getColor(R.color.openhab_orange),
-                    getResources().getColor(R.color.openhab_orange)));
-        }
     }
 }
