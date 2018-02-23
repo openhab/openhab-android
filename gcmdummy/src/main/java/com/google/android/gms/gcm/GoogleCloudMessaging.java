7f1b6379c1badd2d882ddeb8611c1348fef62b47
Danny Baumann <dannybaumann@web.de>
2018-02-23 08:23:03 +0100

    Remove gcmdummy project.
    It's obsolete now that we have the dummy CloudMessagingHelper in the
    foss flavor.
    
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- gcmdummy/src/main/java/com/google/android/gms/gcm/GoogleCloudMessaging.java
+++
@@ -1,9 +0,0 @@
-package com.google.android.gms.gcm;
-
-import android.content.Context;
-
-public class GoogleCloudMessaging {
-    public static GoogleCloudMessaging getInstance(Context applicationContext) {
-        return null;
-    }
-}
