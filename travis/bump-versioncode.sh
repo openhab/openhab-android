058df79005f165f8057e2d64d5bcda3bb03e87d0
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-08 19:42:10 +0100

    Use git hash if no tag is available
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- travis/bump-versioncode.sh
+++ travis/bump-versioncode.sh
@@ -11,9 +11,15 @@ manifest="mobile/src/main/AndroidManifest.xml"
 currentVersionCode=$(grep 'android:versionCode' $manifest | sed -r 's/(.*)"(.*)"/\2/')
 let currentVersionCode++
 
+if [ -z "$TRAVIS_TAG" ]
+then
+    TRAVIS_TAG=$(git rev-parse HEAD)
+    echo "Tag is empty, use git hash ($TRAVIS_TAG) instead"
+fi
+
 if [ -z "$currentVersionCode" ] || [ -z "$TRAVIS_TAG" ]
 then
-    echo "Code or tag are empty! Don't bump anything"
+    echo "Code ($currentVersionCode) or tag ($TRAVIS_TAG) are empty! Don't bump anything."
 else
     echo "New version code is $currentVersionCode and name $TRAVIS_TAG"
     sed --in-place -r "s/android:versionCode=\"(.*)\"/android:versionCode=\"${currentVersionCode}\"/" $manifest
