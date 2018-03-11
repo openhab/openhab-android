a8355df4a389ebeec5f23294c1559fcbe8cbb3a1
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-11 10:51:34 +0100

    Build either stable or beta
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- travis/commit-version-and-metadata.sh
+++ travis/commit-version-and-metadata.sh
@@ -23,10 +23,30 @@ chmod 400 travis/key
 echo "Git create master branch"
 git checkout -b master
 echo "Git add remote"
-git remote add github git@github.com:openhab/openhab-android
+git remote add github git@github.com:openhab/openhab-android > /dev/null 2>&1
 echo "Git fetch"
-git fetch github
+git fetch github > /dev/null 2>&1
 echo "Git push master"
-git push --quiet github master
+git push --quiet github master > /dev/null 2>&1
 echo "Git push F-Droid tag"
-git push --quiet github ${TRAVIS_TAG}-fdroid
+git push --quiet github ${TRAVIS_TAG}-fdroid > /dev/null 2>&1
+
+releaseFlavor="stable"
+if $(echo "$TRAVIS_TAG" | grep -q "beta")
+then
+    echo "Beta"
+    releaseFlavor="beta"
+fi
+
+openssl aes-256-cbc -K $encrypted_903a93ed2309_key -iv $encrypted_903a93ed2309_iv -in keystore.enc -out keystore -d
+cp $TRAVIS_BUILD_DIR/keystore $HOME
+mkdir $HOME/apks_to_deploy
+cp mobile/build/outputs/apk/full${releaseFlavor}/release/mobile-full-${releaseFlavor}-release-unsigned.apk $HOME/apks_to_deploy
+cp mobile/build/outputs/apk/foss${releaseFlavor}/release/mobile-foss-${releaseFlavor}-release-unsigned.apk $HOME/apks_to_deploy
+cd $HOME/apks_to_deploy
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $HOME/keystore -storepass $storepass mobile-full-${releaseFlavor}-release-unsigned.apk sign
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $HOME/keystore -storepass $storepass mobile-foss-${releaseFlavor}-release-unsigned.apk sign
+jarsigner -verify mobile-full-${releaseFlavor}-release-unsigned.apk
+jarsigner -verify mobile-foss-${releaseFlavor}-release-unsigned.apk
+${ANDROID_HOME}/build-tools/25.0.2/zipalign -v 4 mobile-full-${releaseFlavor}-release-unsigned.apk openhab-android.apk
+${ANDROID_HOME}/build-tools/25.0.2/zipalign -v 4 mobile-foss-${releaseFlavor}-release-unsigned.apk openhab-android-foss.apk
