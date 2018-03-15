c83a2cef9c478f67c73466e31509d5ae89882884
Danny Baumann <dannybaumann@web.de>
2018-03-15 08:52:55 +0100

    Fix color mapper test.
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- mobile/src/androidTest/java/org/openhab/habdroid/ui/WidgetAdapterTest.java
+++ mobile/src/androidTest/java/org/openhab/habdroid/ui/WidgetAdapterTest.java
@@ -1,55 +1,37 @@
 package org.openhab.habdroid.ui;
 
 import android.content.Context;
-import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 import org.openhab.habdroid.R;
 import org.openhab.habdroid.ui.OpenHABWidgetAdapter.ColorMapper;
-import org.openhab.habdroid.util.Constants;
 
 public class WidgetAdapterTest {
     private Context context = InstrumentationRegistry.getTargetContext();
-    @Before
-    public void setupTest() {
 
-    }
     @Test
     public void testColorMappingDarkTheme() {
-        PreferenceManager
-                .getDefaultSharedPreferences(context)
-                .edit()
-                .putString(Constants.PREFERENCE_THEME,
-                        context.getString(R.string.theme_value_basic_ui_dark))
-                .commit();
-
+        context.setTheme(R.style.HABDroid_Basic_ui_dark);
         ColorMapper colorMapper = new ColorMapper(context);
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("#ffffff"));
+        assertEquals(Integer.valueOf(0xffffffff), colorMapper.mapColor("#ffffff"));
         assertEquals(null, colorMapper.mapColor("#fffzzz"));
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("white"));
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("red"));
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("yellow"));
+        assertEquals(Integer.valueOf(0xffffffff), colorMapper.mapColor("white"));
+        assertEquals(Integer.valueOf(0xffff0000), colorMapper.mapColor("red"));
+        assertEquals(Integer.valueOf(0xffffff00), colorMapper.mapColor("yellow"));
     }
 
     @Test
     public void testColorMappingBrightTheme() {
-        PreferenceManager
-                .getDefaultSharedPreferences(context)
-                .edit()
-                .putString(Constants.PREFERENCE_THEME,
-                        context.getString(R.string.theme_value_basic_ui))
-                .commit();
-
+        context.setTheme(R.style.HABDroid_Basic_ui);
         ColorMapper colorMapper = new ColorMapper(context);
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("#ffffff"));
+        assertEquals(Integer.valueOf(0xffffffff), colorMapper.mapColor("#ffffff"));
         assertEquals(null, colorMapper.mapColor("#fffzzz"));
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("white"));
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("red"));
-        assertEquals(Integer.valueOf(2), colorMapper.mapColor("yellow"));
+        assertEquals(Integer.valueOf(0xff000000), colorMapper.mapColor("white"));
+        assertEquals(Integer.valueOf(0xffff0000), colorMapper.mapColor("red"));
+        assertEquals(Integer.valueOf(0xfffdd835), colorMapper.mapColor("yellow"));
     }
 }
