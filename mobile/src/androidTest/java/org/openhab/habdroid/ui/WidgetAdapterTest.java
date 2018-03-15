857f349b999cdc764622e156042e6edf3e9085de
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-15 21:27:22 +0100

    Add messages to tests
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/androidTest/java/org/openhab/habdroid/ui/WidgetAdapterTest.java
+++ mobile/src/androidTest/java/org/openhab/habdroid/ui/WidgetAdapterTest.java
@@ -17,21 +17,21 @@ public class WidgetAdapterTest {
     public void testColorMappingDarkTheme() {
         context.setTheme(R.style.HABDroid_Basic_ui_dark);
         ColorMapper colorMapper = new ColorMapper(context);
-        assertEquals(Integer.valueOf(0xffffffff), colorMapper.mapColor("#ffffff"));
-        assertEquals(null, colorMapper.mapColor("#fffzzz"));
-        assertEquals(Integer.valueOf(0xffffffff), colorMapper.mapColor("white"));
-        assertEquals(Integer.valueOf(0xffff0000), colorMapper.mapColor("red"));
-        assertEquals(Integer.valueOf(0xffffff00), colorMapper.mapColor("yellow"));
+        assertEquals("Map #ffffff", Integer.valueOf(0xffffffff), colorMapper.mapColor("#ffffff"));
+        assertEquals("Must return \"null\" for invalid colors", null, colorMapper.mapColor("#fffzzz"));
+        assertEquals("Map white => #ffffff in dark themes", Integer.valueOf(0xffffffff), colorMapper.mapColor("white"));
+        assertEquals("Map red => #ff0000 in dark themes", Integer.valueOf(0xffff0000), colorMapper.mapColor("red"));
+        assertEquals("Map yellow => #ffff00 in dark themes", Integer.valueOf(0xffffff00), colorMapper.mapColor("yellow"));
     }
 
     @Test
     public void testColorMappingBrightTheme() {
         context.setTheme(R.style.HABDroid_Basic_ui);
         ColorMapper colorMapper = new ColorMapper(context);
-        assertEquals(Integer.valueOf(0xffffffff), colorMapper.mapColor("#ffffff"));
-        assertEquals(null, colorMapper.mapColor("#fffzzz"));
-        assertEquals(Integer.valueOf(0xff000000), colorMapper.mapColor("white"));
-        assertEquals(Integer.valueOf(0xffff0000), colorMapper.mapColor("red"));
-        assertEquals(Integer.valueOf(0xfffdd835), colorMapper.mapColor("yellow"));
+        assertEquals("Map #ffffff", Integer.valueOf(0xffffffff), colorMapper.mapColor("#ffffff"));
+        assertEquals("Must return \"null\" for invalid colors", null, colorMapper.mapColor("#fffzzz"));
+        assertEquals("Map white => #000000 in bright themes", Integer.valueOf(0xff000000), colorMapper.mapColor("white"));
+        assertEquals("Map red => #ff0000 in bright themes", Integer.valueOf(0xffff0000), colorMapper.mapColor("red"));
+        assertEquals("Map yellow => #fdd835 in bright themes", Integer.valueOf(0xfffdd835), colorMapper.mapColor("yellow"));
     }
 }
