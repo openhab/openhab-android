9a686c3210757c1e279f9099ac1cf6e6f0ccfb67
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-17 15:26:53 +0100

    Add path to apk to Screengrabfile
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- travis/build-and-test-apk.sh
+++ travis/build-and-test-apk.sh
@@ -2,22 +2,29 @@
 
 set -e
 
-releaseFlavor="Stable"
+releaseFlavor="stable"
+releaseFlavorCapital="Stable"
+packageName="org.openhab.habdroid"
+
 if $(echo "$TRAVIS_TAG" | grep -q "beta")
 then
     echo "Beta"
-    releaseFlavor="Beta"
+    releaseFlavor="beta"
+    releaseFlavorCapital="Beta"
+    packageName="org.openhab.habdroid.beta"
 fi
 
 echo "Build"
-time ./gradlew :mobile:assemble{Foss,Full}${releaseFlavor}{Debug,Release}
+time ./gradlew :mobile:assemble{Foss,Full}${releaseFlavorCapital}{Debug,Release}
 echo "Unit tests"
-time ./gradlew :mobile:test{Foss,Full}${releaseFlavor}ReleaseUnitTest
+time ./gradlew :mobile:test{Foss,Full}${releaseFlavorCapital}ReleaseUnitTest
 echo "Android tests"
-time ./gradlew :mobile:connected{Foss,Full}${releaseFlavor}DebugAndroidTest
+time ./gradlew :mobile:connected{Foss,Full}${releaseFlavorCapital}DebugAndroidTest
 echo "Jacoco coverage reports"
-time ./gradlew :mobile:jacocoTest{Foss,Full}${releaseFlavor}DebugUnitTestReport
+time ./gradlew :mobile:jacocoTest{Foss,Full}${releaseFlavorCapital}DebugUnitTestReport
 
+echo "app_package_name('${packageName}')" >> fastlane/Screengrabfile
+echo "app_apk_path('mobile/build/outputs/apk/full${releaseFlavorCapital}/debug/mobile-full-${releaseFlavor}-debug.apk')" >> fastlane/Screengrabfile
 echo "Screengrab"
 time fastlane screengrab
 
