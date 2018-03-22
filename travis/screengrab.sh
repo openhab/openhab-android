3b514370e0c75843af803bb074abec5fd39525d1
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-22 18:06:11 +0100

    Workaround for #794
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- travis/screengrab.sh
+++ travis/screengrab.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+sed -i -e 's|maven.fabric.io/public|repo1.maven.org/maven2|' -e /fabric/d -e '/google-services/d' mobile/build.gradle
 
-releaseFlavor="stable"
-releaseFlavorCapital="Stable"
-packageName="org.openhab.habdroid"
+./gradlew assembleFullStable assembleAndroidTest
 
-if $(echo "$TRAVIS_TAG" | grep -q "beta")
-then
-    echo "Beta"
-    releaseFlavor="beta"
-    releaseFlavorCapital="Beta"
-    packageName="org.openhab.habdroid.beta"
-fi
-
-sed -i -e /fabric/d -e '/google-services/d' build.gradle
-
-echo "app_package_name('${packageName}')" >> fastlane/Screengrabfile
-echo "tests_package_name('${packageName}')" >> fastlane/Screengrabfile
-echo "app_apk_path('mobile/build/outputs/apk/full${releaseFlavorCapital}/debug/mobile-full-${releaseFlavor}-debug.apk')" >> fastlane/Screengrabfile
-echo "tests_apk_path('mobile/build/outputs/apk/full${releaseFlavorCapital}/debug/mobile-full-${releaseFlavor}-debug.apk')" >> fastlane/Screengrabfile
-echo "Screengrab"
 time fastlane screengrab
 
 git add . && git diff --staged
