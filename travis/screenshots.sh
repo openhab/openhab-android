2cc424af7129c291ea1b6f1bc859d532d6dfb71e
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-29 21:12:30 +0200

    Make it ready for merge
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- travis/screenshots.sh
+++ travis/screenshots.sh
@@ -3,7 +3,7 @@
 if [ -z "$TRAVIS_TAG" ]
 then
     echo "No tag present, do nothing"
-    #exit
+    exit
 fi
 
 bash travis/bump-versioncode.sh
@@ -29,7 +29,6 @@ echo "Install fastlane"
 gem install fastlane screengrab
 
 patch=$(echo "$TRAVIS_TAG" | sed -r 's/([0-9]+)\.([0-9]+)\.([0-9]+)-(.*)/\3/')
-patch=11
 if [ $((patch%3)) -eq 0 ]
 then
     bash travis/start-emulator.sh phone
