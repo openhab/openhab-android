486cf7c6ed1a91e1f4d2a8ffd0c094c0fe8653cb
mueller-ma <mueller-ma@users.noreply.github.com>
2018-04-01 20:19:36 +0200

    Remove discovery intro slide
    The user will see an error message if discovery failed, so I don't see
    the point any longer why we should tell him about it before.
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/IntroActivity.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/IntroActivity.java
@@ -31,9 +31,6 @@ public class IntroActivity extends AppIntro {
         addOHSlide(R.string.intro_themes,
                 R.string.intro_themes_description,
                 R.drawable.themes);
-        addOHSlide(R.string.intro_discovery,
-                R.string.intro_discovery_summary,
-                R.drawable.demo_mode);
         // Change bar color
         setBarColor(colorOpenHABOrange);
         setSeparatorColor(colorOpenHABOrangeDark);
