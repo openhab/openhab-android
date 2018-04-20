aa87ccb27c265ee4bcb86e39ddee776d5ef670b8
mueller-ma <mueller-ma@users.noreply.github.com>
2018-04-20 12:15:01 +0200

    Improve Python script (#837)
    * Use xml attributes
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Use android style for strings.xml
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Create directory first
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Unescape strings
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * s/found/find/
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Revert commits
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * s/found/find/
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Add encoding=utf-8
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Fix failing script
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Remove encoding=utf8
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Improve behavior on empty strings
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Exit with non zero on errors
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Print lang
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>
    
    * Code formatting
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- assets/store_descriptions/generate_and_validate.py
+++ assets/store_descriptions/generate_and_validate.py
@@ -1,65 +1,82 @@
 #!/usr/bin/env python3
 
-import xml.etree.ElementTree as ET
 import glob
+import os
+import re
 import sys
+import xml.etree.ElementTree as ET
 
+enTree = ET.parse('assets/store_descriptions/en-US/strings.xml')
+enRoot = enTree.getroot()
 
-en_tree = ET.parse('assets/store_descriptions/en-US/strings.xml')
-en_root = en_tree.getroot()
+emptyStringPattern = re.compile("^([ ]+)+$")
+
+exitCode = 0
+
+def getEnglishString(key):
+    return(enRoot.findall(key)[0].text)
 
 def getString(key):
     try:
         string = root.findall(key)[0].text
+        if emptyStringPattern.match(string):
+            string = getEnglishString(key)
     except:
-        string = en_root.findall(key)[0].text
+        string = getEnglishString(key)
     return(string)
 
-strings_files=glob.glob('assets/store_descriptions/*/strings.xml')
-for file in strings_files:
+stringsFiles = glob.glob('assets/store_descriptions/*/strings.xml')
+for file in stringsFiles:
     tree = ET.parse(file)
     root = tree.getroot()
-    lang=file[26:-12]
+    lang = file[26:-12]
+    print("Processing " + lang)
 
-    full_description = getString('intro') + "\n\n"
+    fullDescription = getString('intro') + "\n\n"
     if sys.argv[1] == "fdroid":
-        full_description+= getString('fdroid') + "\n\n"
+        fullDescription += getString('fdroid') + "\n\n"
     elif sys.argv[1] == "fdroidBeta":
-        full_description+= getString('fdroid') + "\n"
-        full_description+= getString('fdroid_beta') + "\n\n"
-    full_description+= "<b>" + getString('important_note') + "</b>\n\n"
-    full_description+= getString('oh_server') + "\n\n"
-    full_description+= getString('whatis') + "\n"
-    full_description+= getString('rules') + "\n\n"
-    full_description+="<b>" + getString('supported') + "</b>\n\n"
-    full_description+= getString('bindings') + "\n\n"
-    full_description+= getString('home_automation_solutions') + "\n"
-    full_description+= getString('lighting') + "\n"
-    full_description+= getString('heating') + "\n"
-    full_description+= getString('home_entertainment') + "\n"
-    full_description+= getString('security') + "\n"
-    full_description+= getString('open_protocols') + "\n"
-    full_description+= getString('special_useCases') + "\n"
-    full_description+= getString('empty_point') + "\n\n"
-    full_description+= "<b>" + getString('oss_community') + "</b>\n\n"
-    full_description+= getString('forum') + "\n"
-    full_description+= getString('report_issues') + "\n"
-    full_description+= getString('translation') + "\n\n"
-    full_description+= "<b>" + getString('foundation') + "</b>\n\n"
-    full_description+= getString('about_foundation') + "\n"
+        fullDescription += getString('fdroid') + "\n"
+        fullDescription += getString('fdroid_beta') + "\n\n"
+    fullDescription += "<b>" + getString('important_note') + "</b>\n\n"
+    fullDescription += getString('oh_server') + "\n\n"
+    fullDescription += getString('whatis') + "\n"
+    fullDescription += getString('rules') + "\n\n"
+    fullDescription += "<b>" + getString('supported') + "</b>\n\n"
+    fullDescription += getString('bindings') + "\n\n"
+    fullDescription += getString('home_automation_solutions') + "\n"
+    fullDescription += getString('lighting') + "\n"
+    fullDescription += getString('heating') + "\n"
+    fullDescription += getString('home_entertainment') + "\n"
+    fullDescription += getString('security') + "\n"
+    fullDescription += getString('open_protocols') + "\n"
+    fullDescription += getString('special_useCases') + "\n"
+    fullDescription += getString('empty_point') + "\n\n"
+    fullDescription += "<b>" + getString('oss_community') + "</b>\n\n"
+    fullDescription += getString('forum') + "\n"
+    fullDescription += getString('report_issues') + "\n"
+    fullDescription += getString('translation') + "\n\n"
+    fullDescription += "<b>" + getString('foundation') + "</b>\n\n"
+    fullDescription += getString('about_foundation') + "\n"
 
-    if len(full_description) > 4000:
-        print("Description of " + lang + " too long!")
+    if len(fullDescription) > 4000:
+        print("Full description of " + lang + " is too long: " + str(len(fullDescription)) + " chars")
+        exitCode += 1
 
+    shortDescription = getString('short_description')
+    if len(shortDescription) > 80:
+        print("Short description of " + lang + " is too long: " + str(len(fullDescription)) + " chars")
+        exitCode += 1
 
-    short_description = getString('short_description')
-    if len(short_description) > 80:
-        print("Description of " + lang + " too long!")
+    newpath = r'fastlane/metadata/android/' + lang + '/'
+    if not os.path.exists(newpath):
+            os.makedirs(newpath)
 
     f = open('fastlane/metadata/android/' + lang + '/full_description.txt', 'w')
-    f.write(full_description)
+    f.write(fullDescription)
     f.close()
     f = open('fastlane/metadata/android/' + lang + '/short_description.txt', 'w')
-    f.write(short_description)
+    f.write(shortDescription)
     f.close()
 
+exit(exitCode)
