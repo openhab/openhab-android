c1d5c587ee9d5d514158fbf632aefbf4816e19c9
mueller-ma <mueller-ma@users.noreply.github.com>
2018-11-01 18:45:12 +0100

    Fix slow chart loading issue (#1061)
    Fixes #440
    
    This issue can already solved server-side by setting a max width for
    charts in Paper UI, but it's not easy to find for the user.
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/util/Constants.java
+++ mobile/src/main/java/org/openhab/habdroid/util/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
     public static final String PREFERENCE_FIRST_START           = "firstStart";
     public static final String PREFERENCE_SWIPE_REFRESH_EXPLAINED = "swipToRefreshExplained";
     public static final String PREFERENCE_CHART_SCALING       = "chartScalingFactor";
+    public static final String PREFERENCE_CHART_HQ            = "default_openhab_chart_hq";
     public static final String PREFERENCE_ICON_FORMAT         = "iconFormatType";
 
     public static final String SUBSCREEN_LOCAL_CONNECTION     = "default_openhab_local_connection";
