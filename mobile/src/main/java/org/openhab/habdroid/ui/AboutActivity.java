79a167d27b645a99daf23450e9f52169e64dec18
mueller-ma <mueller-ma@users.noreply.github.com>
2018-12-07 20:12:19 +0100

    Fix back arrow behavior in about
    Fixes #1099
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/AboutActivity.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/AboutActivity.java
@@ -73,7 +73,11 @@ public class AboutActivity extends AppCompatActivity implements
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+                getSupportFragmentManager().popBackStack();
+            } else {
+                finish();
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
