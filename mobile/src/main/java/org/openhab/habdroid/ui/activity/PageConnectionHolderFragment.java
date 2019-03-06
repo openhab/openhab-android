a135a1ffa3c6e6653f74cdf03726ecf5b7113c32
maniac103 <dannybaumann@web.de>
2019-03-06 11:52:04 +0100

    Fix SSE cancel behavior. (#1237)
    Don't flag failure to the respective connection handler if the SSE
    onClosed() callback is called due to SSE being canceled instead of retry
    exhaustion. Doing so will 'revive' canceled connection handlers, which
    will cause all kinds of state tracking confusion.
    
    Closes #1228
    
    * Check for stale SSE instance before doing callback.
    
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- mobile/src/main/java/org/openhab/habdroid/ui/activity/PageConnectionHolderFragment.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/activity/PageConnectionHolderFragment.java
@@ -561,7 +561,13 @@ public class PageConnectionHolderFragment extends Fragment {
 
             @Override
             public void onClosed(ServerSentEvent sse) {
-                mFailureCb.handleFailure();
+                // We're only interested in permanent failure here, not in callbacks we caused
+                // ourselves by calling close(), so check for both
+                // - the reporter matching our expectations (mismatch means shutdown was called)
+                // - retry count exhaustion
+                if (mRetries >= MAX_RETRIES && sse == mEventStream) {
+                    mFailureCb.handleFailure();
+                }
             }
 
             @Override
