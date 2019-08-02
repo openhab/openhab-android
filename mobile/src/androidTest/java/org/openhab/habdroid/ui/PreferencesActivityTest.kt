0d0274994607e31f7df93b713c324f3d38a0e0f4
mueller-ma <mueller-ma@users.noreply.github.com>
2019-08-02 23:08:12 +0200

    Code style
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/androidTest/java/org/openhab/habdroid/ui/PreferencesActivityTest.kt
+++ mobile/src/androidTest/java/org/openhab/habdroid/ui/PreferencesActivityTest.kt
@@ -35,11 +35,12 @@ class PreferencesActivityTest {
 
     @Test
     fun testBeautifyHostName() {
-        assertEquals("For invalid urls it should return the input value","abc", beautifyUrl("abc"))
+        assertEquals("For invalid urls it should return the input value", "abc", beautifyUrl("abc"))
         assertEquals("For an empty string it should return an empty string", "", beautifyUrl(""))
         assertEquals("URLs without scheme should treated like one with", "myopenHAB", beautifyUrl("myopenhab.org"))
         assertEquals("For myopenhab.org it should return myopenHAB", "myopenHAB", beautifyUrl("https://myopenhab.org"))
-        assertEquals("For home.myopenhab.org it should return myopenHAB", "myopenHAB", beautifyUrl("https://home.myopenhab.org"))
+        assertEquals("For home.myopenhab.org it should return myopenHAB", "myopenHAB",
+            beautifyUrl("https://home.myopenhab.org"))
         assertEquals("not.myopenhab.org", beautifyUrl("https://not.myopenhab.org"))
         assertEquals("notmyopenhab.org", beautifyUrl("https://notmyopenhab.org"))
         assertEquals("myopenhab.WRONG_TLD", beautifyUrl("https://myopenhab.WRONG_TLD"))
