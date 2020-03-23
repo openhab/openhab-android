f9f7f55dd4f66c00993975db48b25790d7659be9
mueller-ma <mueller-ma@users.noreply.github.com>
2020-03-23 14:24:35 +0100

    Fix dynamic icons (#1879)
    Fixes #1874
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/model/Widget.kt
+++ mobile/src/main/java/org/openhab/habdroid/model/Widget.kt
@@ -124,9 +124,8 @@ data class Widget(
         @Throws(JSONException::class)
         fun updateFromEvent(source: Widget, eventPayload: JSONObject): Widget {
             val item = Item.updateFromEvent(source.item, eventPayload.optJSONObject("item"))
-            val iconName = eventPayload.optStringOrNull("icon")
-            val icon = if (iconName != null)
-                iconName.toOH2WidgetIconResource(item, source.type, source.mappings.isNotEmpty()) else source.icon
+            val iconName = eventPayload.optStringOrFallback("icon", source.icon?.icon)
+            val icon = iconName.toOH2WidgetIconResource(item, source.type, source.mappings.isNotEmpty())
             return Widget(source.id, source.parentId,
                 eventPayload.optString("label", source.label),
                 icon,
