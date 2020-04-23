035cdc9e57d68866647aff2954a9a96e848736c2
mueller-ma <mueller-ma@users.noreply.github.com>
2020-04-23 15:11:41 +0200

    Implement review comments
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/background/BackgroundTasksManager.kt
+++ mobile/src/main/java/org/openhab/habdroid/background/BackgroundTasksManager.kt
@@ -388,7 +388,7 @@ class BackgroundTasksManager : BroadcastReceiver() {
                 var time: String? = if (sender in IGNORED_PACKAGES_FOR_ALARM) {
                     "UNDEF"
                 } else {
-                    (info?.triggerTime ?: "UNDEF").toString()
+                    info?.triggerTime?.toString() ?: "UNDEF"
                 }
 
                 val prefs = context.getPrefs()
