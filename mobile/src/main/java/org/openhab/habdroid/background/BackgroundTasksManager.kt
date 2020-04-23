8f931bd62b962cfec57029af7fa6378fe1a7eb9d
mueller-ma <mueller-ma@users.noreply.github.com>
2020-04-23 17:30:14 +0200

    Alarm clock: Send UNDEF if no alarm is set (#1970)
    Fixes #1964
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/background/BackgroundTasksManager.kt
+++ mobile/src/main/java/org/openhab/habdroid/background/BackgroundTasksManager.kt
@@ -388,7 +388,7 @@ class BackgroundTasksManager : BroadcastReceiver() {
                 var time: String? = if (sender in IGNORED_PACKAGES_FOR_ALARM) {
                     "UNDEF"
                 } else {
-                    (info?.triggerTime ?: 0).toString()
+                    info?.triggerTime?.toString() ?: "UNDEF"
                 }
 
                 val prefs = context.getPrefs()
