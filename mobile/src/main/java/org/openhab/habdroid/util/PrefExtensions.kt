maniac103/openhab.android

Replace item prefix with device identifier
Rename the existing prefix of "Send device info" to "Device identifier"
and make it available for NFC tags and voice command.

Closes #1692
Closes #1951

For NFC tags a new query parameter is added (`d=true`) if the device
identifier should be send instead of the state on the tag. Older clients
will send "UNSUPPORTED" instead.

Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

mobile/src/main/java/org/openhab/habdroid/util/PrefExtensions.kt
@@ -114,6 +114,16 @@ fun SharedPreferences.areSitemapsShownInDrawer(): Boolean {
     return getBoolean(PrefKeys.SHOW_SITEMAPS_IN_DRAWER, false)
 }
 
+fun SharedPreferences.getPrefixForVoice(): String? {
+    val enabled = getBoolean(PrefKeys.DEV_ID_PREFIX_VOICE, false)
+    return if (enabled) getString(PrefKeys.DEV_ID) else null
+}
+
+fun SharedPreferences.getPrefixForBgTasks(): String {
+    val enabled = getBoolean(PrefKeys.DEV_ID_PREFIX_BG_TASKS, true)
+    return if (enabled) getString(PrefKeys.DEV_ID) else ""
+}
+
 fun SharedPreferences.getString(key: String): String {
     return getString(key, "").orEmpty()
 }

openhab/openhab-android

mobile/src/main/java/org/openhab/habdroid/util/PrefExtensions.kt
@@ -120,6 +120,16 @@ fun SharedPreferences.getBackgroundTaskScheduleInMillis(): Long {
     return value.toInt() * 60 * 1000L
 }
 
+fun SharedPreferences.getPrefixForVoice(): String? {
+    val enabled = getBoolean(PrefKeys.DEV_ID_PREFIX_VOICE, false)
+    return if (enabled) getStringOrEmpty(PrefKeys.DEV_ID) else null
+}
+
+fun SharedPreferences.getPrefixForBgTasks(): String {
+    val enabled = getBoolean(PrefKeys.DEV_ID_PREFIX_BG_TASKS, true)
+    return if (enabled) getStringOrEmpty(PrefKeys.DEV_ID) else ""
+}
+
 fun SharedPreferences.getStringOrNull(key: String): String? {
     return getString(key, null)
 }