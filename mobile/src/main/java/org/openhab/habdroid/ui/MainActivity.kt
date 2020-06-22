c0302b36749a2ac03a9ce5c5fa4153ecdaf4d4c0
mueller-ma <mueller-ma@users.noreply.github.com>
2020-06-22 22:49:41 +0200

    Fix UninitializedPropertyAccessException in MainActivity (#2060)
    Fixes #2052
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/MainActivity.kt
+++ mobile/src/main/java/org/openhab/habdroid/ui/MainActivity.kt
@@ -121,7 +121,7 @@ class MainActivity : AbstractBaseActivity(), ConnectionFactory.UpdateListener {
     private var drawerIconTintList: ColorStateList? = null
     lateinit var viewPool: RecyclerView.RecycledViewPool
         private set
-    private lateinit var progressBar: ContentLoadingProgressBar
+    private var progressBar: ContentLoadingProgressBar? = null
     private var sitemapSelectionDialog: AlertDialog? = null
     private var lastSnackbar: Snackbar? = null
     var connection: Connection? = null
@@ -895,9 +895,9 @@ class MainActivity : AbstractBaseActivity(), ConnectionFactory.UpdateListener {
 
     fun setProgressIndicatorVisible(visible: Boolean) {
         if (visible) {
-            progressBar.show()
+            progressBar?.show()
         } else {
-            progressBar.hide()
+            progressBar?.hide()
         }
     }
 
