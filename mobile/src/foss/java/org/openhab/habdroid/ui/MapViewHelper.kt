openhab/openhab-android

commit 8a685c988cb7350a63d435124958ef47289ef763

Optimize image loading (#2039)
* Improve chart image caching.

Append 'random' query parameter in WidgetImageView to make sure it
doesn't interfere with both local bitmap caching and checking whether a
given URL is already in the process of being fetched.

Signed-off-by: Danny Baumann <dannybaumann@web.de>

* Don't forcibly fetch images when resuming activities.

PR #1717 intended to force image load when bringing the app from
background to foreground, which unfortunately had the side effect of
fetching the image twice when first loading the page/activity (once when
attaching the view to the window, second on resume). Avoid that by
tweaking the logic a bit: reset 'last refresh' timestamp on pause of
activity, which leads to 'next refresh due time' being in the past on
next resume.

Signed-off-by: Danny Baumann <dannybaumann@web.de>

mobile/src/main/java/org/openhab/habdroid/ui/WidgetAdapter.kt
@@ -886,7 +886,6 @@ class WidgetAdapter(
         connection: Connection
     ) : HeavyDataViewHolder(inflater, parent, R.layout.widgetlist_chartitem, connection), View.OnClickListener {
         private val chart = widgetContentView as WidgetImageView
-        private val random = Random()
         private val prefs: SharedPreferences
         private var refreshRate = 0
         private val density: Int
@@ -911,7 +910,7 @@ class WidgetAdapter(
             }
 
             val chartUrl =
-                widget.toChartUrl(prefs, random, parent.width, chartTheme = chartTheme, density = density) ?: return
+                widget.toChartUrl(prefs, parent.width, chartTheme = chartTheme, density = density) ?: return
             Log.d(TAG, "Chart url = $chartUrl")
             chart.setImageUrl(connection, chartUrl, parent.width, forceLoad = true)
             refreshRate = widget.refresh

mobile/src/main/java/org/openhab/habdroid/ui/widget/WidgetImageView.kt
@@ -31,6 +31,7 @@ import org.openhab.habdroid.R
 import org.openhab.habdroid.core.connection.Connection
 import org.openhab.habdroid.util.CacheManager
 import org.openhab.habdroid.util.HttpClient
+import kotlin.random.Random
 
 class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
     private var scope: CoroutineScope? = null
@@ -41,6 +42,7 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
     private var originalScaleType: ScaleType? = null
     private var originalAdjustViewBounds: Boolean = false
     private val emptyHeightToWidthRatio: Float
+    private val addRandomnessToUrl: Boolean
     private var internalLoad: Boolean = false
     private var lastRequest: HttpImageRequest? = null
 
@@ -53,6 +55,7 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
             fallback = getDrawable(R.styleable.WidgetImageView_fallback)
             progressDrawable = getDrawable(R.styleable.WidgetImageView_progressIndicator)
             emptyHeightToWidthRatio = getFraction(R.styleable.WidgetImageView_emptyHeightToWidthRatio, 1, 1, 0f)
+            addRandomnessToUrl = getBoolean(R.styleable.WidgetImageView_addRandomnessToUrl, false)
             recycle()
         }
 
@@ -70,9 +73,13 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
         val client = connection.httpClient
         val actualUrl = client.buildUrl(url)
 
-        if (lastRequest?.isActiveForUrl(actualUrl) == true) {
-            // We're already in the process of loading this image, thus there's nothing to do
-            return
+        if (actualUrl == lastRequest?.url) {
+            if (lastRequest?.isActive() == true) {
+                // We're already in the process of loading this image, thus there's nothing to do
+                return
+            }
+        } else {
+            lastRefreshTimestamp = 0
         }
 
         cancelCurrentLoad()
@@ -137,8 +144,7 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         scope = CoroutineScope(Dispatchers.Main + Job())
-        val request = lastRequest
-        if (request != null) {
+        lastRequest?.let { request ->
             if (!request.hasCompleted()) {
                 request.execute(false)
             } else {
@@ -154,12 +160,10 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
     }
 
     fun startRefreshing(refreshDelayInMs: Int) {
-        cancelRefresh()
+        refreshJob?.cancel()
+        refreshJob = null
         refreshInterval = refreshDelayInMs.toLong()
-
-        refreshJob = scope?.launch {
-            lastRefreshTimestamp = SystemClock.uptimeMillis()
-            lastRequest?.execute(true)
+        if (lastRequest?.isActive() != true) {
             scheduleNextRefresh()
         }
     }
@@ -168,6 +172,7 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
         refreshJob?.cancel()
         refreshJob = null
         refreshInterval = 0
+        lastRefreshTimestamp = 0
     }
 
     private fun setBitmapInternal(bitmap: Bitmap) {
@@ -186,7 +191,6 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
         val timeToNextRefresh = refreshInterval + lastRefreshTimestamp - SystemClock.uptimeMillis()
         refreshJob = scope?.launch {
             delay(timeToNextRefresh)
-            lastRefreshTimestamp = SystemClock.uptimeMillis()
             lastRequest?.execute(true)
         }
     }
@@ -220,27 +224,43 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
 
     private inner class HttpImageRequest(
         private val client: HttpClient,
-        private val url: HttpUrl,
+        val url: HttpUrl,
         private val size: Int,
         private val timeoutMillis: Long
     ) {
         private var job: Job? = null
+        private var lastRandomness = Random.Default.nextInt()
 
         fun execute(avoidCache: Boolean) {
+            if (job?.isActive == true) {
+                // Nothing to do, we're still in the process of downloading
+                return
+            }
+
             Log.i(TAG, "Refreshing image at $url, avoidCache $avoidCache")
             val cachingMode = if (avoidCache)
                 HttpClient.CachingMode.AVOID_CACHE
             else
                 HttpClient.CachingMode.FORCE_CACHE_IF_POSSIBLE
 
+            val actualUrl = if (addRandomnessToUrl) {
+                if (avoidCache) {
+                    lastRandomness = Random.Default.nextInt()
+                }
+                url.newBuilder().setQueryParameter("random", lastRandomness.toString()).build()
+            } else {
+                url
+            }
+
             job = scope?.launch(Dispatchers.Main) {
                 try {
-                    val bitmap = client.get(url.toString(),
+                    val bitmap = client.get(actualUrl.toString(),
                         timeoutMillis = timeoutMillis, caching = cachingMode)
                         .asBitmap(size)
                         .response
                     setBitmapInternal(bitmap)
                     CacheManager.getInstance(context).cacheBitmap(url, bitmap)
+                    lastRefreshTimestamp = System.currentTimeMillis()
                     scheduleNextRefresh()
                 } catch (e: HttpClient.HttpException) {
                     removeProgressDrawable()
@@ -257,8 +277,8 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
             return job?.isCompleted == true
         }
 
-        fun isActiveForUrl(url: HttpUrl): Boolean {
-            return job?.isActive == true && this.url == url
+        fun isActive(): Boolean {
+            return job?.isActive == true
         }
     }
 

mobile/src/main/java/org/openhab/habdroid/model/Widget.kt
@@ -86,7 +86,6 @@ data class Widget(
 
     fun toChartUrl(
         prefs: SharedPreferences,
-        random: Random,
         width: Int,
         height: Int = width / 2,
         chartTheme: CharSequence?,
@@ -104,7 +103,6 @@ data class Widget(
             .appendQueryParameter(if (item.type === Item.Type.Group) "groups" else "items", item.name)
             .appendQueryParameter("dpi", actualDensity.toInt() / resDivider)
             .appendQueryParameter("period", forcedPeriod)
-            .appendQueryParameter("random", random.nextInt())
 
         if (service.isNotEmpty()) {
             chartUrl.appendQueryParameter("service", service)

mobile/src/main/java/org/openhab/habdroid/ui/ChartActivity.kt
@@ -41,7 +41,6 @@ class ChartActivity : AbstractBaseActivity(), SwipeRefreshLayout.OnRefreshListen
     private lateinit var widget: Widget
     private lateinit var chartTheme: CharSequence
     private var density: Int = 0
-    private val random = Random()
     private var showLegend: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,7 +72,10 @@ class ChartActivity : AbstractBaseActivity(), SwipeRefreshLayout.OnRefreshListen
 
     override fun onResume() {
         super.onResume()
-        onRefresh()
+        loadChartImage(false)
+        if (widget.refresh > 0 && !isDataSaverActive()) {
+            chart.startRefreshing(widget.refresh)
+        }
     }
 
     override fun onPause() {
@@ -145,6 +147,11 @@ class ChartActivity : AbstractBaseActivity(), SwipeRefreshLayout.OnRefreshListen
     }
 
     override fun onRefresh() {
+        loadChartImage(true)
+        swipeLayout.isRefreshing = false
+    }
+
+    private fun loadChartImage(force: Boolean) {
         val connection = ConnectionFactory.usableConnectionOrNull
         if (connection == null) {
             finish()
@@ -158,7 +165,6 @@ class ChartActivity : AbstractBaseActivity(), SwipeRefreshLayout.OnRefreshListen
 
         val chartUrl = widget.toChartUrl(
             getPrefs(),
-            random,
             chart.width,
             chart.height,
             chartTheme,
@@ -168,11 +174,7 @@ class ChartActivity : AbstractBaseActivity(), SwipeRefreshLayout.OnRefreshListen
         ) ?: return
 
         Log.d(TAG, "Load chart with url $chartUrl")
-        chart.setImageUrl(connection, chartUrl, chart.width, forceLoad = true)
-        if (widget.refresh > 0 && !isDataSaverActive()) {
-            chart.startRefreshing(widget.refresh)
-        }
-        swipeLayout.isRefreshing = false
+        chart.setImageUrl(connection, chartUrl, chart.width, forceLoad = force)
     }
 
     private fun updateHasLegendButtonState(item: MenuItem) {

mobile/src/main/res/layout/activity_chart.xml
@@ -32,6 +32,7 @@
             android:layout_height="match_parent"
             android:adjustViewBounds="true"
             android:scaleType="fitCenter"
-            app:progressIndicator="@drawable/ic_image_loading_themed" />
+            app:progressIndicator="@drawable/ic_image_loading_themed"
+            app:addRandomnessToUrl="true" />
     </androidx.swiperefreshlayout.widget.SwipeRefreshLayout>
 </androidx.constraintlayout.widget.ConstraintLayout>

mobile/src/main/res/layout/widgetlist_chartitem.xml
@@ -17,7 +17,8 @@
         android:scaleType="fitCenter"
         android:layout_margin="4dip"
         app:emptyHeightToWidthRatio="50%"
-        app:progressIndicator="@drawable/ic_image_loading_themed" />
+        app:progressIndicator="@drawable/ic_image_loading_themed"
+        app:addRandomnessToUrl="true" />
 
     <include layout="@layout/widgetlist_data_saver" />
 

mobile/src/main/res/values/attrs.xml
@@ -10,6 +10,7 @@
         <attr name="fallback" format="reference" />
         <attr name="progressIndicator" format="reference" />
         <attr name="emptyHeightToWidthRatio" format="fraction" />
+        <attr name="addRandomnessToUrl" format="boolean" />
     </declare-styleable>
 
     <declare-styleable name="ItemUpdatingPreference">

commit 0fed151a44c8c843e2732c494478141b0b635652

Fix image refresh and image/video pause/resume behavior (#2071)
* Use correct clock for generating image 'last refresh' timestamps.

This broke in #2039 ... we need to consistently use monotonic time since
bootup.

Closes #2066

* Fix start/stop behavior of some widgets.

On resume after being sent to background, RecyclerView re-bound some
widgets, which caused the ViewHolders to stop, but not re-start with the
previous implementation.
Additionally, for video widgets, the previous media item equality check
did not work, which caused the video to needlessly be reloaded after
resuming from background.

Signed-off-by: Danny Baumann <dannybaumann@web.de>

mobile/src/foss/java/org/openhab/habdroid/ui/MapViewHelper.kt
@@ -66,7 +66,6 @@ object MapViewHelper {
         Marker.OnMarkerDragListener {
         private val mapView = baseMapView as MapView
         private val handler: Handler = Handler()
-        private var started: Boolean = false
         override val dialogManager = WidgetAdapter.DialogManager()
 
         init {
@@ -97,18 +96,12 @@ object MapViewHelper {
             }
         }
 
-        override fun start() {
-            if (!started) {
-                mapView.onResume()
-                started = true
-            }
+        override fun onStart() {
+            mapView.onResume()
         }
 
-        override fun stop() {
-            if (started) {
-                mapView.onPause()
-                started = false
-            }
+        override fun onStop() {
+            mapView.onPause()
         }
 
         override fun onMarkerDragStart(marker: Marker) {

mobile/src/full/java/org/openhab/habdroid/ui/MapViewHelper.kt
@@ -53,7 +53,6 @@ object MapViewHelper {
         GoogleMap.OnMarkerDragListener {
         private val mapView = baseMapView as MapView
         private var map: GoogleMap? = null
-        private var started: Boolean = false
         override val dialogManager = WidgetAdapter.DialogManager()
 
         init {
@@ -77,22 +76,16 @@ object MapViewHelper {
             map?.applyPositionAndLabel(boundItem, labelView.text, 15.0f, false)
         }
 
-        override fun start() {
-            super.start()
-            if (!started) {
-                mapView.onStart()
-                mapView.onResume()
-                started = true
-            }
+        override fun onStart() {
+            super.onStart()
+            mapView.onStart()
+            mapView.onResume()
         }
 
-        override fun stop() {
-            super.stop()
-            if (started) {
-                mapView.onPause()
-                mapView.onStop()
-                started = false
-            }
+        override fun onStop() {
+            super.onStop()
+            mapView.onPause()
+            mapView.onStop()
         }
 
         override fun onMarkerDragStart(marker: Marker) {

mobile/src/main/java/org/openhab/habdroid/ui/WidgetAdapter.kt
@@ -87,7 +87,6 @@ import org.openhab.habdroid.util.orDefaultIfEmpty
 import java.util.Calendar
 import java.util.HashMap
 import java.util.Locale
-import java.util.Random
 import java.util.concurrent.CancellationException
 import java.util.concurrent.Executor
 import kotlin.math.abs
@@ -197,7 +196,7 @@ class WidgetAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.stop()
+        val wasStarted = holder.stop()
         holder.bind(items[position])
         if (holder is FrameViewHolder) {
             holder.setShownAsFirst(position == 0)
@@ -207,6 +206,9 @@ class WidgetAdapter(
             isLongClickable = true
             isActivated = selectedPosition == position
             setOnClickListener(this@WidgetAdapter)
+        }
+        if (wasStarted) {
+            holder.start()
         }
     }
 
@@ -310,9 +312,26 @@ class WidgetAdapter(
         @LayoutRes layoutResId: Int
     ) : RecyclerView.ViewHolder(inflater.inflate(layoutResId, parent, false)) {
         open val dialogManager: DialogManager? = null
+        var started = false
+            private set
+
         abstract fun bind(widget: Widget)
-        open fun start() {}
-        open fun stop() {}
+        fun start() {
+            if (!started) {
+                onStart()
+                started = true
+            }
+        }
+        fun stop(): Boolean {
+            if (!started) {
+                return false
+            }
+            onStop()
+            started = false
+            return true
+        }
+        open fun onStart() {}
+        open fun onStop() {}
         open fun handleRowClick() {}
     }
 
@@ -626,7 +645,7 @@ class WidgetAdapter(
             }
         }
 
-        override fun start() {
+        override fun onStart() {
             if (refreshRate > 0 && !itemView.context.isDataSaverActive()) {
                 imageView.startRefreshing(refreshRate)
             } else {
@@ -634,7 +653,7 @@ class WidgetAdapter(
             }
         }
 
-        override fun stop() {
+        override fun onStop() {
             imageView.cancelRefresh()
         }
     }
@@ -916,7 +935,7 @@ class WidgetAdapter(
             refreshRate = widget.refresh
         }
 
-        override fun start() {
+        override fun onStart() {
             if (refreshRate > 0 && !itemView.context.isDataSaverActive()) {
                 chart.startRefreshing(refreshRate)
             } else {
@@ -924,7 +943,7 @@ class WidgetAdapter(
             }
         }
 
-        override fun stop() {
+        override fun onStop() {
             chart.cancelRefresh()
         }
 
@@ -962,45 +981,51 @@ class WidgetAdapter(
                     .build()
             }
 
-            if (mediaItem != mediaPlayer.currentMediaItem) {
-                mediaPlayer.reset()
-                if (mediaItem != null) {
-                    mediaPlayer.setMediaItem(mediaItem)
-                    val prepareFuture = mediaPlayer.prepare()
-                    prepareFuture.addListener(Runnable {
-                        val code = try {
-                            prepareFuture.get().resultCode
-                        } catch (e: CancellationException) {
-                            Log.d(TAG, "Task was canceled")
-                            BaseResult.RESULT_ERROR_UNKNOWN
-                        }
-                        Log.d(TAG, "Media player returned $code")
-                        loadingIndicator.isVisible = false
-                        if (code >= 0) {
-                            // No error code
-                            return@Runnable
-                        }
-
-                        val label =
-                            widget.label.orDefaultIfEmpty(itemView.context.getString(R.string.widget_type_video))
-                        errorViewHint.text = itemView.context.getString(R.string.error_video_player, label)
-                        errorView.isVisible = true
-                    }, Executor {
-                        Handler(Looper.getMainLooper()).post(it)
-                    })
-                }
+            val currentUri = (mediaPlayer.currentMediaItem as? UriMediaItem)?.uri
+            if (currentUri == mediaItem?.uri) {
+                return
             }
 
-            start()
+            mediaPlayer.reset()
+            if (mediaItem == null) {
+                return
+            }
+
+            mediaPlayer.setMediaItem(mediaItem)
+            val prepareFuture = mediaPlayer.prepare()
+            prepareFuture.addListener(Runnable {
+                val code = try {
+                    prepareFuture.get().resultCode
+                } catch (e: CancellationException) {
+                    Log.d(TAG, "Task was canceled")
+                    BaseResult.RESULT_ERROR_UNKNOWN
+                }
+                Log.d(TAG, "Media player returned $code")
+                loadingIndicator.isVisible = false
+                if (code >= 0) {
+                    // No error code
+                    if (started) {
+                        mediaPlayer.play()
+                    }
+                    return@Runnable
+                }
+
+                val label =
+                    widget.label.orDefaultIfEmpty(itemView.context.getString(R.string.widget_type_video))
+                errorViewHint.text = itemView.context.getString(R.string.error_video_player, label)
+                errorView.isVisible = true
+            }, Executor {
+                Handler(Looper.getMainLooper()).post(it)
+            })
         }
 
-        override fun start() {
+        override fun onStart() {
             if (mediaPlayer.currentMediaItem != null) {
                 mediaPlayer.play()
             }
         }
 
-        override fun stop() {
+        override fun onStop() {
             if (mediaPlayer.currentMediaItem != null) {
                 mediaPlayer.pause()
             }
@@ -1178,11 +1203,11 @@ class WidgetAdapter(
             streamer = widget.url?.let { MjpegStreamer(imageView, connection, it) }
         }
 
-        override fun start() {
+        override fun onStart() {
             streamer?.start()
         }
 
-        override fun stop() {
+        override fun onStop() {
             streamer?.stop()
         }
     }

mobile/src/main/java/org/openhab/habdroid/ui/widget/WidgetImageView.kt
@@ -189,6 +189,7 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
             return
         }
         val timeToNextRefresh = refreshInterval + lastRefreshTimestamp - SystemClock.uptimeMillis()
+        Log.d(TAG, "Scheduling next refresh for ${lastRequest?.url} in $timeToNextRefresh ms")
         refreshJob = scope?.launch {
             delay(timeToNextRefresh)
             lastRequest?.execute(true)
@@ -260,7 +261,7 @@ class WidgetImageView constructor(context: Context, attrs: AttributeSet?) : AppC
                         .response
                     setBitmapInternal(bitmap)
                     CacheManager.getInstance(context).cacheBitmap(url, bitmap)
-                    lastRefreshTimestamp = System.currentTimeMillis()
+                    lastRefreshTimestamp = SystemClock.uptimeMillis()
                     scheduleNextRefresh()
                 } catch (e: HttpClient.HttpException) {
                     removeProgressDrawable()