f7c4a6f3d7a60e88589a740b1302f9e247d7e045
mueller-ma <mueller-ma@users.noreply.github.com>
2020-11-01 14:00:57 +0100

    Hide SSE failure snackbar when opening HABPanel
    SSE failures of the sitemap don't impair the refresh of HABPanel.
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/MainActivity.kt
+++ mobile/src/main/java/org/openhab/habdroid/ui/MainActivity.kt
@@ -1037,6 +1037,7 @@ class MainActivity : AbstractBaseActivity(), ConnectionFactory.UpdateListener {
     }
 
     private fun openWebViewUi(ui: WebViewUi) {
+        hideSnackbar(SNACKBAR_TAG_SSE_ERROR)
         controller.showWebViewUi(ui)
         drawerToggle.isDrawerIndicatorEnabled = false
     }
