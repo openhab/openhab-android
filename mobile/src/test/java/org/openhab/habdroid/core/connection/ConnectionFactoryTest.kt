6f5517fa49ff39486afdd4e5a347120d9c506f46
mueller-ma <mueller-ma@users.noreply.github.com>
2020-12-08 15:18:42 +0100

    Fix unit test
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/test/java/org/openhab/habdroid/core/connection/ConnectionFactoryTest.kt
+++ mobile/src/test/java/org/openhab/habdroid/core/connection/ConnectionFactoryTest.kt
@@ -43,8 +43,6 @@ import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
-import org.openhab.habdroid.core.connection.exception.NetworkNotAvailableException
-import org.openhab.habdroid.core.connection.exception.NoUrlInformationException
 import org.openhab.habdroid.util.PrefKeys
 
 class ConnectionFactoryTest {
