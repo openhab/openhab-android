5bfa2236b42a1fa1bf100eb56d2fc25f5a5b53e6
mueller-ma <mueller-ma@users.noreply.github.com>
2021-01-28 17:14:16 +0100

    Reduce "invalid URL" log messages
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/util/ExtensionFuncs.kt
+++ mobile/src/main/java/org/openhab/habdroid/util/ExtensionFuncs.kt
@@ -99,7 +99,9 @@ fun String.obfuscate(clearTextCharCount: Int = 3): String {
 }
 
 fun String?.toNormalizedUrl(): String? {
-    this ?: return null
+    if (isNullOrEmpty()) {
+        return null
+    }
     return try {
         val url = this
             .replace("\n", "")
@@ -108,7 +110,7 @@ fun String?.toNormalizedUrl(): String? {
             .toString()
         if (url.endsWith("/")) url else "$url/"
     } catch (e: IllegalArgumentException) {
-        Log.d(Util.TAG, "normalizeUrl(): invalid URL '$this'")
+        Log.d(Util.TAG, "toNormalizedUrl(): Invalid URL '$this'")
         null
     }
 }
