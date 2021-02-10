81fcf382b9c063706cff536a192264e2b70e1e79
mueller-ma <mueller-ma@users.noreply.github.com>
2021-02-10 22:50:24 +0100

    Retry unit tests
    See #2385
    
    Source: https://mromeh.com/2017/12/04/implement-retry-in-unit-test/
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

---
+++ mobile/src/test/java/org/openhab/habdroid/testUtils/RetryRule.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.openhab.habdroid.testUtils
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class RetryRule : TestRule {
+    private var currentAttempt = 0
+    private var lastError: Throwable? = null
+
+    override fun apply(base: Statement, description: Description): Statement {
+        description.getAnnotation(Retry::class.java) ?: return base
+
+        return object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                while (currentAttempt < 3) {
+                    try {
+                        base.evaluate()
+                        return
+                    } catch (t: Throwable) {
+                        lastError = t
+                        currentAttempt++
+                        runBlocking {
+                            delay(1000)
+                        }
+                    }
+                }
+                throw Exception(lastError)
+            }
+        }
+    }
+}
+
+@Target(AnnotationTarget.FUNCTION)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class Retry()
