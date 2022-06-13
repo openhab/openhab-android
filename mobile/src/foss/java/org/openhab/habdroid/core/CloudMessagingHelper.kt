openhab/openhab-android

commit 417acc2d98e4f862c5881dad2302bd27ea06d796

Replace isFull/isFoss checks by more meaningful functions (#2771)
Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

mobile/src/main/java/org/openhab/habdroid/ui/PreferencesActivity.kt
@@ -348,7 +348,7 @@ class PreferencesActivity : AbstractBaseActivity() {
                 false
             }
 
-            if (Util.isFlavorFoss) {
+            if (CloudMessagingHelper.needsPollingForNotifications(requireContext())) {
                 preferenceScreen.removePreferenceRecursively(PrefKeys.NOTIFICATION_STATUS_HINT)
             } else {
                 preferenceScreen.removePreferenceRecursively(PrefKeys.FOSS_NOTIFICATIONS_ENABLED)
@@ -399,13 +399,13 @@ class PreferencesActivity : AbstractBaseActivity() {
                 preferenceScreen.removePreferenceRecursively(PrefKeys.TASKER_PLUGIN_ENABLED)
             }
 
-            if (Util.isFlavorFoss) {
-                preferenceScreen.removePreference(crashReporting)
-            } else {
+            if (CrashReportingHelper.canBeDisabledByUser()) {
                 crashReporting.setOnPreferenceClickListener {
                     CrashReportingHelper.initialize(requireActivity().application)
                     true
                 }
+            } else {
+                preferenceScreen.removePreference(crashReporting)
             }
 
             viewLogPref.setOnPreferenceClickListener { preference ->

mobile/src/foss/java/org/openhab/habdroid/util/CrashReportingHelper.kt
@@ -60,6 +60,8 @@ object CrashReportingHelper {
         return ACRA.isACRASenderServiceProcess()
     }
 
+    fun canBeDisabledByUser() = false
+
     fun d(tag: String, message: String, remoteOnly: Boolean = false, exception: Exception? = null) {
         if (!remoteOnly) {
             Log.d(tag, message, exception)

mobile/src/full/java/org/openhab/habdroid/util/CrashReportingHelper.kt
@@ -34,9 +34,9 @@ object CrashReportingHelper {
     }
 
     // Only required for ACRA
-    fun isCrashReporterProcess(): Boolean {
-        return false
-    }
+    fun isCrashReporterProcess() = false
+
+    fun canBeDisabledByUser() = true
 
     fun d(tag: String, message: String, remoteOnly: Boolean = false, exception: Exception? = null) {
         FirebaseCrashlytics.getInstance().log("D/$tag: $message; ${exception?.stackTraceToString()}")

mobile/src/main/java/org/openhab/habdroid/util/Util.kt
@@ -23,8 +23,6 @@ object Util {
 
     val isFlavorStable get() = BuildConfig.FLAVOR.lowercase(Locale.ROOT).contains("stable")
     val isFlavorBeta get() = !isFlavorStable
-    val isFlavorFull get() = BuildConfig.FLAVOR.lowercase(Locale.ROOT).contains("full")
-    val isFlavorFoss get() = !isFlavorFull
 
     fun isEmulator(): Boolean {
         val isEmulator = Build.FINGERPRINT.startsWith("generic") ||

commit 2fb4c1261bf6aff7822dd213e2feb38a76b04e96

Fix showing FOSS notification pref
Bug introduced by https://github.com/openhab/openhab-android/commit/417acc2d98e4f862c5881dad2302bd27ea06d796#diff-213515902329aa3da737b520a0fe64f82225bc3e5f223077189e8ca49acd755eR351

Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

mobile/src/foss/java/org/openhab/habdroid/core/CloudMessagingHelper.kt
@@ -34,6 +34,8 @@ object CloudMessagingHelper {
     @Suppress("UNUSED_PARAMETER")
     fun onNotificationSelected(context: Context, intent: Intent) {}
 
+    fun isPollingBuild() = true
+
     fun needsPollingForNotifications(context: Context) =
         context.getPrefs().getBoolean(PrefKeys.FOSS_NOTIFICATIONS_ENABLED, false)
 

mobile/src/full/java/org/openhab/habdroid/core/CloudMessagingHelper.kt
@@ -48,6 +48,8 @@ object CloudMessagingHelper {
         }
     }
 
+    fun isPollingBuild() = false
+
     fun needsPollingForNotifications(@Suppress("UNUSED_PARAMETER") context: Context) = false
 
     fun pollForNotifications(@Suppress("UNUSED_PARAMETER") context: Context) {

mobile/src/main/java/org/openhab/habdroid/ui/PreferencesActivity.kt
@@ -338,7 +338,7 @@ class PreferencesActivity : AbstractBaseActivity() {
                 false
             }
 
-            if (CloudMessagingHelper.needsPollingForNotifications(requireContext())) {
+            if (CloudMessagingHelper.isPollingBuild()) {
                 preferenceScreen.removePreferenceRecursively(PrefKeys.NOTIFICATION_STATUS_HINT)
             } else {
                 preferenceScreen.removePreferenceRecursively(PrefKeys.FOSS_NOTIFICATIONS_ENABLED)