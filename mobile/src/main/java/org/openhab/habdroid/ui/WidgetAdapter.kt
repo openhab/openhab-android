f03a34814217f630ce4fe233541e19f2fa3982b8
mueller-ma <mueller-ma@users.noreply.github.com>
2023-04-17 22:28:37 +0200

    Add setting to hide icons (#3333)
    Closes #3331
    
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/WidgetAdapter.kt
+++ mobile/src/main/java/org/openhab/habdroid/ui/WidgetAdapter.kt
@@ -78,6 +78,7 @@ import org.openhab.habdroid.ui.widget.WidgetSlider
 import org.openhab.habdroid.util.CacheManager
 import org.openhab.habdroid.util.HttpClient
 import org.openhab.habdroid.util.MjpegStreamer
+import org.openhab.habdroid.util.PrefKeys
 import org.openhab.habdroid.util.beautify
 import org.openhab.habdroid.util.determineDataUsagePolicy
 import org.openhab.habdroid.util.getChartTheme
@@ -415,7 +416,11 @@ class WidgetAdapter(
                 valueView.isVisible = !widget.stateFromLabel.isNullOrEmpty()
                 valueView.applyWidgetColor(widget.valueColor, colorMapper)
             }
-            iconView.loadWidgetIcon(connection, widget, colorMapper)
+            val showIcon = !iconView.context.getPrefs().getBoolean(PrefKeys.SHOW_ICONS, true)
+            iconView.isGone = !showIcon
+            if (showIcon) {
+                iconView.loadWidgetIcon(connection, widget, colorMapper)
+            }
         }
     }
 
