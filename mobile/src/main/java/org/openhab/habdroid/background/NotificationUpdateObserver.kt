openhab/openhab-android

commit 2b15f36f7d944853a1315b729688212364c7b248

Fix a few lint errors (#3643)
Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

mobile/src/main/java/org/openhab/habdroid/background/NotificationUpdateObserver.kt
@@ -70,7 +70,8 @@ internal class NotificationUpdateObserver(context: Context) : Observer<List<Work
                             else -> {}
                         }
                     }
-                } else if (tag.startsWith(BackgroundTasksManager.WORKER_TAG_PREFIX_TILE_ID)) {
+                } else if (tag.startsWith(BackgroundTasksManager.WORKER_TAG_PREFIX_TILE_ID) &&
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     val tileId = tag.substringAfter(BackgroundTasksManager.WORKER_TAG_PREFIX_TILE_ID).toInt()
                     AbstractTileService.requestTileUpdate(context, tileId)
                 }

mobile/src/main/java/org/openhab/habdroid/background/tiles/AbstractTileService.kt
@@ -32,7 +32,6 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
-import java.lang.IllegalArgumentException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
@@ -288,6 +287,7 @@ data class TileData(
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.N)
 fun SharedPreferences.getTileData(id: Int): TileData? {
     val tileString = getString(getPrefKeyForId(id), null) ?: return null
     return try {
@@ -305,6 +305,7 @@ fun SharedPreferences.getTileData(id: Int): TileData? {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.N)
 fun SharedPreferences.Editor.putTileData(id: Int, data: TileData?): SharedPreferences.Editor {
     if (data == null) {
         putString(getPrefKeyForId(id), null)

commit 351376b39de57ef272a0e30673ba8df9b0ab1884

Fix another ktlint style issue
Introduced in #3643.

Signed-off-by: Danny Baumann <dannybaumann@web.de>

mobile/src/main/java/org/openhab/habdroid/background/NotificationUpdateObserver.kt
@@ -43,7 +43,8 @@ internal class NotificationUpdateObserver(context: Context) : Observer<List<Work
         val latestInfoByTag = HashMap<String, WorkInfo>()
         for (info in value) {
             for (tag in info.tags) {
-                if (tag in BackgroundTasksManager.KNOWN_KEYS ||
+                if (
+                    tag in BackgroundTasksManager.KNOWN_KEYS ||
                     tag == BackgroundTasksManager.WORKER_TAG_VOICE_COMMAND ||
                     tag.startsWith(BackgroundTasksManager.WORKER_TAG_PREFIX_NFC) ||
                     tag.startsWith(BackgroundTasksManager.WORKER_TAG_PREFIX_TASKER) ||
@@ -70,8 +71,10 @@ internal class NotificationUpdateObserver(context: Context) : Observer<List<Work
                             else -> {}
                         }
                     }
-                } else if (tag.startsWith(BackgroundTasksManager.WORKER_TAG_PREFIX_TILE_ID) &&
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                } else if (
+                    tag.startsWith(BackgroundTasksManager.WORKER_TAG_PREFIX_TILE_ID) &&
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+                ) {
                     val tileId = tag.substringAfter(BackgroundTasksManager.WORKER_TAG_PREFIX_TILE_ID).toInt()
                     AbstractTileService.requestTileUpdate(context, tileId)
                 }