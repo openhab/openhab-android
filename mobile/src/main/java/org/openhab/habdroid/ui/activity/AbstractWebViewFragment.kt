2fb24c6912527f97c15a2e9fb33c73d84074a7e3
Dan Cunningham <dan@digitaldan.com>
2024-06-07 07:26:21 -0700

    Do not require users to have to manually click videos for playback. (#3696)
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>
    Co-authored-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- mobile/src/main/java/org/openhab/habdroid/ui/activity/AbstractWebViewFragment.kt
+++ mobile/src/main/java/org/openhab/habdroid/ui/activity/AbstractWebViewFragment.kt
@@ -145,6 +145,7 @@ abstract class AbstractWebViewFragment : Fragment(), ConnectionFactory.UpdateLis
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         webView = view.findViewById(R.id.webview)
+        webView?.settings?.mediaPlaybackRequiresUserGesture = false
         webView?.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(view: WebView?, newProgress: Int) {
                 Log.d(TAG, "progressCallback: progress = $newProgress")
