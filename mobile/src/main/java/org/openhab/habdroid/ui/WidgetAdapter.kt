openhab/openhab-android

commit c83c5d4c75c66ce6e87519cd08416cf0c33562bb

Fix icon placement for section switches (#3787)
The 'automagic' skeleton addition caused two issues:
- Misplaced icon in section switches (#3773)
  This was caused by constraints in the widget's ConstraintLayout
  referencing the WidgetImageView's ID, which was no longer present
  after replacing it by the SkeletonLayout
- Skeleton and image shown at the same time (mentioned in #3786)
  This was caused by WidgetAdapter and SkeletonLayout both
  simultaneously modifying the visibility flag of the WidgetImageView,
  again caused by silent replacement of WidgetImageView by
  SkeletonLayout

Fix both issues by changing the approach: Instead of silently replacing
the view, make WidgetImageView inherit from SkeletonLayout and make it
redirect external calls to an internal ImageView instance.

Fixes #3773, #3786

Signed-off-by: Danny Baumann <dannybaumann@web.de>

mobile/src/main/java/org/openhab/habdroid/ui/WidgetAdapter.kt
@@ -1042,11 +1042,12 @@ class WidgetAdapter(
 
             // Make sure images fit into the content frame by scaling
             // them at max 90% of the available height
-            if (initData.parent.height > 0) {
-                imageView.maxHeight = (0.9f * initData.parent.height).roundToInt()
-            } else {
-                imageView.maxHeight = Integer.MAX_VALUE
-            }
+            imageView.setMaxHeight(
+                when {
+                    initData.parent.height > 0 -> (0.9f * initData.parent.height).roundToInt()
+                    else -> Integer.MAX_VALUE
+                }
+            )
             imageView.setImageScalingType(prefs.getImageWidgetScalingType())
 
             if (value != null && value.matches("data:image/.*;base64,.*".toRegex())) {

mobile/src/main/res/layout/activity_chart.xml
@@ -15,19 +15,13 @@
         android:layout_height="match_parent"
         app:layout_behavior="@string/appbar_scrolling_view_behavior">
 
-        <com.faltenreich.skeletonlayout.SkeletonLayout
+        <org.openhab.habdroid.ui.widget.WidgetImageView
+            android:id="@+id/chart"
             android:layout_width="match_parent"
             android:layout_height="match_parent"
-            app:maskColor="?skeletonBackground"
-            app:shimmerColor="?skeletonShimmer">
-            <org.openhab.habdroid.ui.widget.WidgetImageView
-                android:id="@+id/chart"
-                android:layout_width="match_parent"
-                android:layout_height="match_parent"
-                app:imageScalingType="scaleToFitWithViewAdjustment"
-                app:addRandomnessToUrl="true"
-                tools:src="@drawable/day_dream_preview" />
-        </com.faltenreich.skeletonlayout.SkeletonLayout>
+            app:imageScalingType="scaleToFitWithViewAdjustment"
+            app:addRandomnessToUrl="true"
+            tools:src="@drawable/day_dream_preview" />
 
     </androidx.swiperefreshlayout.widget.SwipeRefreshLayout>
 

commit 7ddb28be95f0aee599c3f273f7626587903875dc

Fix MjpegVideoViewHolder instantiation
This broke in #3787

Fixes #3802

Signed-off-by: Danny Baumann <dannybaumann@web.de>

mobile/src/main/java/org/openhab/habdroid/ui/WidgetAdapter.kt
@@ -1704,7 +1704,7 @@ class WidgetAdapter(
 
     class MjpegVideoViewHolder internal constructor(initData: ViewHolderInitData) :
         HeavyDataViewHolder(initData, R.layout.widgetlist_videomjpegitem) {
-        private val imageView = widgetContentView as ImageView
+        private val imageView = widgetContentView as WidgetImageView
         private var streamer: MjpegStreamer? = null
 
         override fun bindAfterDataSaverCheck(widget: Widget) {

mobile/src/main/java/org/openhab/habdroid/util/MjpegStreamer.kt
@@ -14,7 +14,6 @@
 package org.openhab.habdroid.util
 
 import android.util.Log
-import android.widget.ImageView
 import java.io.IOException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,8 +22,9 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.openhab.habdroid.core.connection.Connection
+import org.openhab.habdroid.ui.widget.WidgetImageView
 
-class MjpegStreamer(private val view: ImageView, connection: Connection, private val url: String) {
+class MjpegStreamer(private val view: WidgetImageView, connection: Connection, private val url: String) {
     private val httpClient = connection.httpClient
     private var job: Job? = null
 